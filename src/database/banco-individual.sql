c4307e1550b69b62d2b92c861dc0c4d6c190ff7f
Mauro-Neto <mauro.camposno@sptech.school>
2026-05-18 14:30:12 -0300

    Alterações na interação com o usuário nos cadastros

--- src/database/banco-individual.sql
+++ src/database/banco-individual.sql
@@ -23,3 +23,29 @@ FOREIGN KEY (timeFavorito) REFERENCES time_lol (idTime)
 SELECT * FROM invocador;
 SELECT * FROM time_lol;
 
+SELECT nome,
+SUM(titulosNacionais) as nacionais,
+SUM(titulosInternacionais) as internacionais
+FROM time_lol
+GROUP BY nome
+ORDER BY nacionais+internacionais DESC
+LIMIT 5;
+
+SELECT nome, COUNT(*) as total
+FROM invocador JOIN time_lol ON timeFavorito = idTime
+GROUP BY nome
+ORDER BY total DESC;
+
+SELECT 
+  CASE 
+    WHEN pontuacao BETWEEN 0 AND 20 THEN '0-20'
+    WHEN pontuacao BETWEEN 21 AND 40 THEN '21-40'
+    WHEN pontuacao BETWEEN 41 AND 60 THEN '41-60'
+    WHEN pontuacao BETWEEN 61 AND 80 THEN '61-80'
+    ELSE '81-100'
+  END AS faixa,
+  COUNT(*) AS total
+FROM invocador
+WHERE pontuacao IS NOT NULL
+GROUP BY faixa
+ORDER BY faixa;
